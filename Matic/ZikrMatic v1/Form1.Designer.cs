27c68763d063176ba9f8ec3e479d77b13a8da860
gabilgg07 <gabilgg07@gmail.com>
2023-05-20 10:31:46 +0400

    Voice Assistant Windows Form App

---
+++ Matic/ZikrMatic v1/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace ZikrMatic_v1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resultLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(108, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Live speech:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(109, 252);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(612, 93);
+            this.label2.TabIndex = 1;
+            // 
+            // resultLbl
+            // 
+            this.resultLbl.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.resultLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.resultLbl.Location = new System.Drawing.Point(109, 114);
+            this.resultLbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.resultLbl.Name = "resultLbl";
+            this.resultLbl.Size = new System.Drawing.Size(612, 93);
+            this.resultLbl.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 539);
+            this.Controls.Add(this.resultLbl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label resultLbl;
+    }
+}
+
---
+++ Matic/ZikrMatic v1/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace ZikrMatic_v1
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer speech = new SpeechSynthesizer();
+        System.Media.SoundPlayer music = new System.Media.SoundPlayer();
+        public Form1()
+        {
+            InitializeComponent();
+
+            Choices choices = new Choices();
+            string[] text = File.ReadAllLines(Environment.CurrentDirectory + "//grammar.txt");
+            choices.Add(text);
+            Grammar grammar = new Grammar(new GrammarBuilder(choices));
+            recEngine.LoadGrammar(grammar);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            recEngine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recEngne_SpechRecognized);
+
+            speech.SelectVoiceByHints(VoiceGender.Male);
+        }
+        public int count = 0;
+        private void recEngne_SpechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string result = e.Result.Text;
+            resultLbl.Text += result + ", ";
+
+            if (result == "Allah" || result == "Hello Kabil" || result == "What time is it"|| result == "Open google"|| result == "Open my gmail"||
+                result == "Close Google Chrome"|| result == "Open my folder"|| result == "Close My Folder"|| result == "Play My Music"||
+                result == "Stop"|| result == "Shut down")
+            {
+                if (result == "Hello Kabil")
+                {
+                    result = "Hi, how can I help you?";
+                }
+
+                if (result == "What time is it")
+                {
+                    result = "It is currently" + DateTime.Now.ToLongTimeString();
+                }
+
+                if (result == "Open google")
+                {
+                    System.Diagnostics.Process.Start("https://www.google.com/");
+                    result = "Google Opening";
+                }
+
+                if (result == "Open my gmail")
+                {
+                    System.Diagnostics.Process.Start("https://mail.google.com/mail/u/0/#inbox");
+                    result = "Gmail Opening";
+                }
+
+                if (result == "Close Google Chrome")
+                {
+                    System.Diagnostics.Process[] close = System.Diagnostics.Process.GetProcessesByName("chrome");
+                    foreach (System.Diagnostics.Process p in close)
+                    {
+                        p.Kill();
+                    }
+                    result = "Chrome closing";
+                }
+
+                if (result == "Open my folder")
+                {
+                    System.Diagnostics.Process.Start(@"D:\Codes\gitProjects");
+                    result = "Folder Opening";
+                }
+
+                if (result == "Close My Folder")
+                {
+                    System.Diagnostics.Process[] close = System.Diagnostics.Process.GetProcessesByName("explorer");
+                    foreach (System.Diagnostics.Process p in close)
+                    {
+                        p.Kill();
+                    }
+                    result = "Folder closing";
+                }
+
+                if (result == "Play My Music")
+                {
+                    music.SoundLocation = @"C:\Users\user\Desktop\[MP3DOWNLOAD.TO] [OFFICIAL VIDEO] Hallelujah - Pentatonix-HQ.wav";
+                    music.Play();
+                    result = "";
+                }
+
+                if (result == "Stop")
+                {
+                    speech.SpeakAsyncCancelAll();
+                    music.Stop();
+                    result = "";
+                }
+
+                if (result == "Shut up")
+                {
+                    speech.SpeakAsyncCancelAll();
+                }
+
+                if (result == "Shut down")
+                {
+                    Application.Exit();
+                }
+
+                speech.SpeakAsync(result);
+                label2.Text = result;
+            }
+            else if (result == "ALLAH")
+            {
+                count++;
+                result = count.ToString();
+                label2.Text = result;
+            }
+            else
+            {
+                speech.SpeakAsyncCancelAll();
+            }
+        }
+    }
+}
